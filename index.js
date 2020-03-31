03492f8396ee1d1786382c2e6b3fa8d282b72ef3
Eduardo Medina <emedina@alset.com.mx>
2020-03-30 20:35:59 -0600

    Genres and Home routes

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 const express = require('express');
 const app = express();
-const Joi = require('@hapi/joi');
+
+//routes
+const genres = require('./routes/genres');
+const home = require('./routes/home');
 
 //Middleware
 app.use(express.json());
 
-const genres = [
-    {id:1, name:'Action'},
-    {id:2, name:'Cartoons'},
-    {id:3, name:'Anime'}
-];
-
-//Server
-app.get('/vidly/',(req, res)=> {
-    res.send('Hello from Vidly API');
-});
-
-app.get('/vidly/api/genres',(req, res)=> {
-    res.send(genres);
-});
+app.use('/vidly/api/genres',genres);
+app.use('/vidly/api/',home);
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=> console.log(`Listening on port ${port}...`));
